0685267e703ddc45b0d47783fd71165795e30921
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-05-29 00:03:50 +0500

    5235

--- Сервер/middlewares/games.js
+++ Сервер/middlewares/games.js
@@ -73,8 +73,18 @@ const checkEmptyFields = async (req, res, next) => {
     next();
     return;
   } 
-
-  else {
+  if (
+    !req.body.title ||
+    !req.body.description ||
+    !req.body.image ||
+    !req.body.link ||
+    !req.body.developer
+  ) {
+    // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
+    // а ответим кодом 400 — данные неверны.
+    res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
+  } else {
     // Если всё в порядке, то передадим управление следующим миддлварам
     next();
   }
@@ -89,7 +99,11 @@ const checkIfCategoriesAvaliable = async (req, res, next) => {
     return;
   } 
 
-  else {
+    // Проверяем наличие жанра у игры
+  if (!req.body.categories || req.body.categories.length === 0) {
+    res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
+  } else {
     next();
   }
 };
