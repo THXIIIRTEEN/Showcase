947f3e4f5fada063cedc9804e8de2a8a01c34420
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-05-28 22:06:39 +0500

    Update games.js

--- Сервер/middlewares/games.js
+++ Сервер/middlewares/games.js
@@ -68,6 +68,12 @@ const updateGame = async (req, res, next) => {
 };
 
 const checkEmptyFields = async (req, res, next) => {
+
+  if(req.isVoteRequest) {
+    next();
+    return;
+  } 
+
   if (
     !req.body.title ||
     !req.body.description ||
@@ -88,13 +94,18 @@ const checkEmptyFields = async (req, res, next) => {
 // Файл middlewares/games.js
 
 const checkIfCategoriesAvaliable = async (req, res, next) => {
-  // Проверяем наличие жанра у игры
-if (!req.body.categories || req.body.categories.length === 0) {
-  res.setHeader("Content-Type", "application/json");
-      res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
-} else {
-  next();
-}
+
+  if(req.isVoteRequest) {
+    next();
+    return;
+  }   
+    // Проверяем наличие жанра у игры
+  if (!req.body.categories || req.body.categories.length === 0) {
+    res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
+  } else {
+    next();
+  }
 };
 
 // Файл middlewares/games.js
