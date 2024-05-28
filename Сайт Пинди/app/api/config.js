625c52f5598bcddb8d8a1be1d3478730f55a2475
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-05-28 20:10:44 +0500

    Update config.js

--- Сайт Пинди/app/api/config.js
+++ Сайт Пинди/app/api/config.js
@@ -2,6 +2,6 @@ export const BASE_URL = 'https://18karatservera.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
+  auth: `${BASE_URL}/login`,
   me: `${BASE_URL}/me`,
 }
