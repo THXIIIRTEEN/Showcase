bbefc92d4a3f946c80d6bfcd876e39b05dd003e7
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-05-28 23:19:09 +0500

    Update games.js

--- Сервер/middlewares/games.js
+++ Сервер/middlewares/games.js
@@ -73,19 +73,8 @@ const checkEmptyFields = async (req, res, next) => {
     next();
     return;
   } 
-
-  if (
-    !req.body.title ||
-    !req.body.description ||
-    !req.body.image ||
-    !req.body.link ||
-    !req.body.developer
-  ) {
-    // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
-    // а ответим кодом 400 — данные неверны.
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
-  } else {
+  
+  else {
     // Если всё в порядке, то передадим управление следующим миддлварам
     next();
   }
@@ -100,10 +89,7 @@ const checkIfCategoriesAvaliable = async (req, res, next) => {
     return;
   }   
     // Проверяем наличие жанра у игры
-  if (!req.body.categories || req.body.categories.length === 0) {
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
-  } else {
+  else {
     next();
   }
 };
