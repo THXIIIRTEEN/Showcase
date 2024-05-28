fa6aafb809b9f8df4ef7690b1f7f8b433405fd89
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-05-28 23:58:11 +0500

    Update games.js

--- Сервер/middlewares/games.js
+++ Сервер/middlewares/games.js
@@ -67,46 +67,46 @@ const updateGame = async (req, res, next) => {
   }
 };
 
-const checkEmptyFields = async (req, res, next) => {
+// const checkEmptyFields = async (req, res, next) => {
 
-  if (req.isVoteRequest) {
-    next();
-    return;
-  } 
-  if (
-    !req.body.title ||
-    !req.body.description ||
-    !req.body.image ||
-    !req.body.link ||
-    !req.body.developer
-  ) {
-    // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
-    // а ответим кодом 400 — данные неверны.
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
-  } else {
-    // Если всё в порядке, то передадим управление следующим миддлварам
-    next();
-  }
-};
+//   if (req.isVoteRequest) {
+//     next();
+//     return;
+//   } 
+//   if (
+//     !req.body.title ||
+//     !req.body.description ||
+//     !req.body.image ||
+//     !req.body.link ||
+//     !req.body.developer
+//   ) {
+//     // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
+//     // а ответим кодом 400 — данные неверны.
+//     res.setHeader("Content-Type", "application/json");
+//         res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
+//   } else {
+//     // Если всё в порядке, то передадим управление следующим миддлварам
+//     next();
+//   }
+// };
 
-// Файл middlewares/games.js
+// // Файл middlewares/games.js
 
-const checkIfCategoriesAvaliable = async (req, res, next) => {
+// const checkIfCategoriesAvaliable = async (req, res, next) => {
 
-  if(req.isVoteRequest) {
-    next();
-    return;
-  } 
+//   if(req.isVoteRequest) {
+//     next();
+//     return;
+//   } 
 
-    // Проверяем наличие жанра у игры
-  if (!req.body.categories || req.body.categories.length === 0) {
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
-  } else {
-    next();
-  }
-};
+//     // Проверяем наличие жанра у игры
+//   if (!req.body.categories || req.body.categories.length === 0) {
+//     res.setHeader("Content-Type", "application/json");
+//         res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
+//   } else {
+//     next();
+//   }
+// };
 
 // Файл middlewares/games.js
 
