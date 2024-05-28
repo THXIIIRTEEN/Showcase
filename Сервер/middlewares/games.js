eb4b330b043922f1ca265dcfea66601bf57743c6
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-05-29 00:42:27 +0500

    Update games.js

--- Сервер/middlewares/games.js
+++ Сервер/middlewares/games.js
@@ -47,7 +47,10 @@ const createGame = async (req, res, next) => {
 
 const findGameById = async (req, res, next) => {
   try {
-      req.game = await games.findById(req.params.id);
+      req.game = await games
+      .findById(req.params.id)
+      .populate("categories")
+      .populate("users");
   next();
   } catch (error) {
       res.status(404).send({ message: "Game not found" });
